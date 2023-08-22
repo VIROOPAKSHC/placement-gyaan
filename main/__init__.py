092af050c10b9678617a553b7523dbb29605f1b8
Chekuri Viroopaksh <chekuriviroopaksh@gmail.com>
2023-08-22 17:57:09 +0530

    Home Page built

--- main/__init__.py
+++ main/__init__.py
@@ -23,15 +23,17 @@ app.security = Security(app, user_datastore)
 app.config["SECURITY_EMAIL_VALIDATOR_ARGS"] = {"check_deliverability": False}
 api=Api(app)
 jwt=JWTManager(app)
+
 with app.app_context():
     init_db()
     app.security.datastore.find_or_create_role(
-        name="student",description="User with Student Role",permissions={"user-read","user-write"}
+        name="senior",description="User with Senior Role",permissions={"user-read","user-write"}
     )
+    
     db_session.commit()
     if not app.security.datastore.find_user(email="test@me.com"):
         app.security.datastore.create_user(email="test@me.com",username="testuser",
-        password=hash_password("password"), roles=["student"],attributes=["yearsOfExperience-5","company-Willings Inc."])
+        password=hash_password("password"), roles=["senior"],attributes=["yearsOfExperience-5","company-Willings Inc."])
     
 
     db_session.commit()
--- main/controllers.py
+++ main/controllers.py
@@ -1,26 +1,31 @@
 from main import app
+from main.database import db_session
+from main.models import *
 from flask_security import Security, current_user, auth_required, hash_password, \
      SQLAlchemySessionUserDatastore, permissions_accepted,login_required
-from flask import render_template,render_template_string
-
+from flask import render_template,render_template_string,redirect
 @app.route('/')
 def index():
-  if current_user.is_active:
-    return "IS ACTIVE"+"{}".format(current_user.get_id())
-  else:
-    return "NOT ACTIVE"
+  return redirect("/home")
 
 @app.route('/hi')
 @login_required
 def hi():
   return " ".format(dir(current_user))
 
-@app.route('/favicon.ico') 
-def favicon(): 
-  return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# @app.route('/favicon.ico') 
+# def favicon(): 
+#   return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route("/home")
 @auth_required()
 def user_home():
-    return render_template_string("Hello {{ current_user.email }} you are a user!")
+    # print(dir(current_user))
+    # print("Current user : ")
+    # print(current_user.attributes)
+    # print(db_session.query())
+    # print(current_user)
+    # role=Role.query.filter_by(id=(RolesUsers.query.filter_by(user_id=current_user.id).first().role_id)).first()
+    # print(role.id,role.name,role.description,role.permissions)
+    return render_template("home.html")
